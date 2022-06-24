14ebe01d4a48ee12c31b9440a45327afa10d304c
Mirjamil <exapl@.com>
2022-06-25 00:31:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+"use script";
+// let incr = 10;
+// let decr = 10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5 % 2);
+// console.log(5*5 =='25');
+// const isage = true;
+// const isfols = true;
+// const ischecked = false;
+// console.log( isage && isfols || !ischecked  );
+console.log(2+2*2 !== '6' );
